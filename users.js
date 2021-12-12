ca7c5de39808d4018674a3c804194c641121c00d
Natevs <43425836+Natevs@users.noreply.github.com>
2021-12-12 11:59:46 -0600

    Add files via upload

---
+++ users.js
@@ -0,0 +1,23 @@
+//Good?
+const express = require('express');
+const router = express.Router();
+const userController = require('../controller/users');
+const passportService = require('../../config/passport');
+const passport = require('passport');
+const requireAuth = passport.authenticate('jwt', { session: false });
+
+const requireLogin = passport.authenticate('local', { session: false });
+
+router.get('/users', requireAuth,userController.getUsers);//needs event handlers
+
+router.get('/users/:id', requireAuth,userController.getUsersByID);
+
+router.post('/users', requireAuth,userController.updateUsers);
+
+router.put('/', requireAuth,userController.createUsers);
+
+router.delete('/users/:id', requireAuth,userController.deleteUsers);
+
+router.route('/users/login').post(requireLogin, login);
+
+module.exports = router;
